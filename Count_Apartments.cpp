8fe77b81f03d5c0ba06e76d9c1ac82a38f641386
FardinAnnoy <fardin121127@gmail.com>
2024-02-26 13:57:11 +0600

    apartment

--- Count_Apartments.cpp
+++ Count_Apartments.cpp
@@ -1,48 +1,42 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int n, m;
-char a[20][20];
-bool visited[20][20];
+int n,m;
+bool vis[1005][1005];
+char a[1005][1005];
 
-int dx[] = {0, 0, -1, 1};
-int dy[] = {-1, 1, 0, 0};
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
-void dfs(int x, int y) {
-  visited[x][y] = true;
+void dfs(int si,int sj ){
+    vis[si][sj]=true;
+    for(int i=0;i<4;i++){
+        int ci=si+d[i].first;
+        int cj=sj+d[i].second;
 
-  for (int i = 0; i < 4; i++) {
-    int nx = x + dx[i];
-    int ny = y + dy[i];
-
-    if (0 <= nx && nx < n && 0 <= ny && ny < m && a[nx][ny] == '.' && !visited[nx][ny]) {
-      dfs(nx, ny);
+        if( 0<=ci && ci<n && 0<=cj && cj<m && a[ci][cj]=='.' && !vis[ci][cj]){
+            dfs(ci,cj);
+        }
     }
-  }
 }
 
-int main() {
-  cin >> n >> m;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      cin >> a[i][j];
+int main(){
+    cin>>n>>m;
+   
+   for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        cin>>a[i][j];
     }
-  }
+   }
+   int count=0;
 
-  int apartmentCount = 0;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (a[i][j] == '.' && !visited[i][j]) {
-        dfs(i, j);
-        apartmentCount++;
-      }
+   for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+if (a[i][j]=='.' && !vis[i][j]){
+    dfs(i,j);
+    count++;
+}
     }
-  }
+   }
+   cout<<count<<endl;
 
-  cout << apartmentCount << endl;
-
-  return 0;
 }
